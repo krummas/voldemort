b7be5afc789f192a1eba80854581190371d90a72
Marcus Eriksson <marcuse@localhost.localdomain>
2010-02-17 20:37:48 +0100

    add REST interface

---
+++ src/java/voldemort/server/rest/VoldemortRestResource.java
@@ -0,0 +1,8 @@
+package voldemort.server.rest;
+
+/**
+ * Created by IntelliJ IDEA. User: marcuse Date: Feb 13, 2010 Time: 8:42:31 PM To change this template use File |
+ * Settings | File Templates.
+ */
+public class VoldemortRestResource {
+}
